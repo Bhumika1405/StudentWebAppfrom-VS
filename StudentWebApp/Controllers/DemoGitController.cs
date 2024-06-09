7074b650fe3a2d01b9a9e601514d55b3b6bef628
Bhumika1405 <bhumi0272@gmail.com>
2024-06-09 17:00:57 +0530

    new controller

---
+++ StudentWebApp/Controllers/DemoGitController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentWebApp.Controllers
+{
+    public class DemoGitController : Controller
+    {
+        // GET: DemoGit
+        public ActionResult Index()
+        {
+            return View("this is a demo Controller");
+        }
+    }
+}
